f4b648277fa1d58c21555e1992b52c2c55081125
Alexandre Konioukhov <alexan1@gmail.com>
2018-08-10 13:18:34 -0400

    Palindrome

---
+++ Balindrome.Shared/Palindrome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Balindrome.Shared
+{
+    class Palindrome
+    {
+        public int ID { get; set; }
+        public string Phrase { get; set; }
+    }
+}
